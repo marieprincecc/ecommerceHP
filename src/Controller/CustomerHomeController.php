7f949046d0e9258727ae209dc27ffe0712a48e6e
Marie princecc <reveolution.de.saveur@gmail.com>
2021-12-07 17:29:33 +0100

    F prix carroussel page produit

--- src/Controller/CustomerHomeController.php
+++ src/Controller/CustomerHomeController.php
@@ -3,17 +3,25 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CustomerHomeController extends AbstractController
 {
-    #[Route('/', name: 'customer_home')]
-    public function index(CategoryRepository $categoryRepository): Response
+    /**
+     * @Route("/", name="customer_home")
+     */
+    public function index(CategoryRepository $categoryRepository,ProductRepository $productRepository): Response
     {
+        $categories = $categoryRepository->findAll();
 
-        $categories = $categoryRepository->findall();
-        return $this->render('customer/index.html.twig');
+        $products = $productRepository->findBy([],null,8);
+
+        return $this->render('customer/index.html.twig',[
+            'categories' => $categories,
+            'products' => $products
+        ]);
     }
 }
